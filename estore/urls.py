86f1d026ef3542f6ad5c7b37b5765549dc586723
darrenwu1058 <strix@LAPTOP-0FNCUIPR.localdomain>
2019-05-10 20:19:48 +0800

    Add product image

--- estore/urls.py
+++ estore/urls.py
@@ -1,8 +1,14 @@
+from django.conf import settings
 from django.urls import path
+from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
-    path('', views.product_list, name='product_list'),
-    path('create', views.product_create, name='product_create'),
+    path('', views.ProductList.as_view(), name='product_list'),
+    path('create', views.ProductCreate.as_view(), name='product_create'),
+    path('<int:pk>/update', views.ProductUpdate.as_view(), name='product_update'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
